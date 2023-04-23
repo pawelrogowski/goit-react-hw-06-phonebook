7c7f9a0e4c131c6fec2dc79e053cfedd930edf95
Feiron <pawel.rogowski.praca@gmail.com>
2023-04-23 18:22:59 +0200

    refactored the code to use redux-toolkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,91 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import styles from './app.module.css';
+import { addContact, deleteContact, setFilter } from '../app/contactSlice';
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
+  const filter = useSelector(state => state.contacts.filter);
+  const items = useSelector(state => state.contacts.items);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    } else {
-      const defaultContacts = [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ];
-      setContacts(defaultContacts);
-      localStorage.setItem('contacts', JSON.stringify(defaultContacts));
-    }
-  }, []);
-
-  const addContact = (name, number) => {
-    const contactExists = contacts.some(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
-    );
-
-    if (contactExists) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
-
+  const handleAddContact = (name, number) => {
     const id = nanoid();
-    const contact = { id, name, number };
-    setContacts([...contacts, contact]);
-    localStorage.setItem('contacts', JSON.stringify([...contacts, contact]));
+    dispatch(addContact({ id, name, number }));
   };
 
-  const deleteContact = contactId => {
-    setContacts(contacts.filter(contact => contact.id !== contactId));
-    localStorage.setItem(
-      'contacts',
-      JSON.stringify(contacts.filter(contact => contact.id !== contactId))
-    );
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
   };
 
   const handleFilterChange = e => {
     const { value } = e.target;
-    setFilter(value);
+    dispatch(setFilter(value));
   };
 
-  const getFilteredContacts = () => {
-    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-  };
-
-  const filteredContacts = getFilteredContacts();
+  const filteredContacts = items.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
-    <div className={styles['container']}>
+    <div className={styles.container}>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContact} />
+      <ContactForm onSubmit={handleAddContact} />
 
       <h2>Contacts</h2>
       <Filter value={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </div>
   );
 }
 
-App.defaultProps = {
-  contacts: [],
-  filter: '',
-};
-
-App.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  filter: PropTypes.string.isRequired,
-};
 export default App;
